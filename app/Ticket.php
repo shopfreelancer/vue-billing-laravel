85443408ca841b73048be013b13a07d573699207
Hans-Christian Psaar <shopfreelancer@gmail.com>
2018-09-06 14:18:30 +0200

    Removed namedStatus from Ticket Model. Names for status  will be handled in vue frontend

--- app/Ticket.php
+++ app/Ticket.php
@@ -6,18 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    public const NOT_ACTIVE = 0;
-    public const ACTIVE = 1;
-    public const BILLED = 2;
-
-    protected $appends = ['namedStatus'];
     protected $fillable = ['title','description','hours','minutes','status','customer_id'];
-
-    public function getNamedStatusAttribute(){
-        $class = new \ReflectionClass(__CLASS__);
-        $constants = array_flip($class->getConstants());
-
-        $status= (is_null($this->status)) ? 0 : $this->status;
-        return $constants[$status];
-    }
 }
